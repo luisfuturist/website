66ed66201adfcd4d1be4eecb7a96b83d632486d7
Luis Emidio <luisfuturist@gmail.com>
2025-01-05 16:03:18 -0300

    feat: implement custom scroll

---
+++ src/scripts/scroll.ts
@@ -0,0 +1,55 @@
+// Remove the hidden class from elements with the class of remove-hidden-on-load
+document.querySelectorAll(".remove-hidden-on-load").forEach((el) => {
+    el.classList.remove("hidden");
+})
+
+// Disable scroll restoration
+history.scrollRestoration = "manual";
+
+// Scroll to the element with the id of the hash when the back button is clicked
+window.addEventListener("popstate", () => {
+    if (!window.location.hash) return;
+
+    const hash = location.hash.slice(1);
+    const element = document.getElementById("__" + hash);
+
+    if (!element) return;
+
+    element.scrollIntoView({ behavior: "smooth" });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (!window.location.hash) return;
+
+    requestAnimationFrame(() => {
+        // Scroll to the home element
+        const home = document.getElementById("__home")!;
+        home.scrollIntoView({ behavior: "auto" });
+
+        // Animate scroll to the element using scrollIntoView based on hash (id=hash)
+        setTimeout(() => {
+            if (!window.location.hash) return;
+
+            const hash = location.hash.slice(1);
+            const element = document.getElementById("__" + hash);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }, 300);
+    });
+});
+
+// Customize anchor links to scroll to the element with the id of the hash
+document.querySelectorAll(".__anchor").forEach((el) => {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const hash = el.getAttribute("href")!.slice(1);
+        const element = document.getElementById("__" + hash);
+
+        if (!element) return;
+
+        element.scrollIntoView({ behavior: "smooth" });
+        history.pushState(null, "", "#" + hash);
+    });
+});
